0abcbc67355907e7c3d4f1428b10e9593e24a3ce
kotaoue <kotaoue@gmail.com>
2021-01-13 19:05:10 +0900

    Update main.go

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -9,25 +10,29 @@ import (
 )
 
 var s3Config aws.Config
+var getObjectInput s3.GetObjectInput
 
 func init() {
 	s3Config = aws.Config{Region: aws.String("ap-northeast-1")}
+
+	b := flag.String("b", "xxx-bucket", "bucket Name")
+	k := flag.String("k", "xxx-key", "object key")
+	flag.Parse()
+
+	getObjectInput = s3.GetObjectInput{
+		Bucket: aws.String(*b),
+		Key:    aws.String(*k),
+	}
 }
 
 func main() {
 	// sessionの作成
 	sess := session.Must(session.NewSession(&s3Config))
 
-	bucketName := "xxx-bucket"
-	objectKey := "xxx-key"
-
 	// S3 clientを作成
 	svc := s3.New(sess)
 
-	obj, err := svc.GetObject(&s3.GetObjectInput{
-		Bucket: aws.String(bucketName),
-		Key:    aws.String(objectKey),
-	})
+	obj, err := svc.GetObject(&getObjectInput)
 	if err != nil {
 		log.Fatal(err)
 	}
